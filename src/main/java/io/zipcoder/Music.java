cbe8ba1a3f835e5c81ec4b6d001b818694a918c2
santos <santoslopez890@gmail.com>
2023-07-17 18:32:16 -0400

    Play list works and tests pass

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -1,14 +1,35 @@
 package io.zipcoder;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Music {
 
     private String[] playList;
+
 
     public Music(String[] playList){
         this.playList = playList;
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+
+        int songIndex;
+        int count=0;
+       songIndex= Arrays.asList(playList).indexOf(selection);
+        for (int i = 0; i < playList.length; i++) {
+            if(songIndex >= startIndex){
+                songIndex--;
+                count++;
+            }
+            if(songIndex <= songIndex){
+                songIndex++;
+                count++;
+            }
+        }
+
+
+
+
+        return songIndex-1;
     }
 }
